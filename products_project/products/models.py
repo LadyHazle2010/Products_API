e069ab998411297d65bd5843787a4b6d0d46f5e1
T Jackson <sweetladyhazle@gmail.com>
2023-01-27 10:27:54 -0600

    Created Product Model aka Table in MySQL

--- products_project/products/models.py
+++ products_project/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=2000)
+    description = models.CharField(max_length=5000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
+
+
+    
