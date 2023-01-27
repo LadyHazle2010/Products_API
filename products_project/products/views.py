1f1da1b46ca6cbfba813e762700af6b1b4e23e8c
T Jackson <sweetladyhazle@gmail.com>
2023-01-27 15:59:25 -0600

    Created Product URLs + New View Function for Getting Products by Id

--- products_project/products/views.py
+++ products_project/products/views.py
@@ -19,3 +19,12 @@ def products_list(request):
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
    
+@api_view(['GET'])
+def products_detail(request, pk):
+    try:
+        product = Products.objects.get(pk=pk)
+        serializer = Productsserializer(product)
+        return Response(serializer.data)
+
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
